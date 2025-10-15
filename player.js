fbabac90dc83e83ea9df552784b6222dfd718f86
Pahari47 <nachiketapahari00@gmail.com>
2025-10-16 00:06:39 +0530

    player methods added

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+let playerCounter = 0;
 
 export default class Player {
     constructor(name) {
@@ -5,5 +6,25 @@ export default class Player {
         this.x = null;
         this.y = null;
         this.isEliminated = false;
+    }
+
+    static create() {
+        const name = String.fromCharCode(65 + playerCounter++);
+        return new Player(name);
+    }
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    moveToward(destX, destY) {
+        if(this.isEliminated) return;
+
+        const dx = destX - this.x;
+        const dy = destY - this.y;
+
+        this.x += Math.sign(dx);
+        this.y += Math.sign(dy);
     }
 }
